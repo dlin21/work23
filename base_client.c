f43911c5613839e79d1c3c052cb1e8ea4f5b13b8
dlin21 <90690772+dlin21@users.noreply.github.com>
2022-01-07 10:31:52 -0500

    Update base_client.c

--- base_client.c
+++ base_client.c
@@ -1,1 +1,19 @@
+#include "pipe_networking.h"
+
+
+int main() {
+    char line[BUFFER_SIZE];
+
+    int sd = client_connect();
+
+    while (1) {
+        // Prompts user for input
+        printf("Relay Message to Server: ");
+        fgets(line, BUFFER_SIZE, stdin);
+
+        write(sd, line, sizeof(line)); // send data to server
+        read(sd, line, sizeof(line)); // reads processed data from server
+        printf("Response From Server: %s\n", line); // displays processed data to server
+    }
+}
 
