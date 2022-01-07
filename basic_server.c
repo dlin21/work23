3e94e65c82f4a0af6a1c18b5a4dc2a98b05307e3
dlin21 <90690772+dlin21@users.noreply.github.com>
2022-01-07 10:39:29 -0500

    Update basic_server.c

--- basic_server.c
+++ basic_server.c
@@ -1,1 +1,25 @@
+#include "pipe_networking.h"
 
+int main() {
+  int sd = server_setup();
+  
+  while (1) {
+    int from_client = server_connect(sd);
+    int f = fork();
+       
+    if (f) {} 
+        
+    else { 
+      char line[BUFFER_SIZE];
+
+      while (1) { // Prompt client for input
+        if (read(from_client, line, sizeof(line))==0) {
+          close(from_client);
+          break;
+        }
+        for(int i=0; i<strlen(line); i++) line[i] = tolower(line[i]);
+        write(from_client, line, sizeof(line)); // send data to client
+      }
+  }
+  return 0;
+}
