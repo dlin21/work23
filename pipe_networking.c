bf9b3f5bd520b328e3844b7c752ee47dd3686308
dlin21 <90690772+dlin21@users.noreply.github.com>
2022-01-09 20:02:22 -0500

    Update pipe_networking.c

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,22 +1,20 @@
 #include "pipe_networking.h"
 
 int server_setup() {
-    printf("creating socket\n");
     struct addrinfo * hints, * results;
     hints = calloc(1,sizeof(struct addrinfo));
+    
     hints->ai_family = AF_INET;
     hints->ai_socktype = SOCK_STREAM; //TCP socket
     hints->ai_flags = AI_PASSIVE; //only needed on server
     getaddrinfo(NULL, "9845", hints, &results);  //Server sets node to NULL
 
-    printf("binding socket\n");
+    // create socket
     int sd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
     bind(sd, results->ai_addr, results->ai_addrlen);
 
-    printf("listening on socket\n");
     listen(sd, 10);
 
-    printf("freeing stuff\n");
     free(hints);
     freeaddrinfo(results);
 
