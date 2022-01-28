3808ef6e965813c336456d897f18938ebd67b131
dlin21 <90690772+dlin21@users.noreply.github.com>
2022-01-27 22:53:17 -0500

    Update pipe_networking.c

--- pipe_networking.c
+++ pipe_networking.c
@@ -7,13 +7,29 @@ int server_setup() {
     hints->ai_family = AF_INET;
     hints->ai_socktype = SOCK_STREAM; //TCP socket
     hints->ai_flags = AI_PASSIVE; //only needed on server
-    getaddrinfo(NULL, "9845", hints, &results);  //Server sets node to NULL
-
+    int err = getaddrinfo(NULL, "9845", hints, &results);  //Server sets node to NULL
+    if(err == -1){
+        printf("server: unable to get IP info...\n");
+        return -1;
+    }
     // create socket
     int sd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
-    bind(sd, results->ai_addr, results->ai_addrlen);
+    if(sd == -1){
+        printf("server: unable to make socket...\n");
+        return -1;
+    }
+    int err = bind(sd, results->ai_addr, results->ai_addrlen);
+    if(err == -1){
+        printf("server: unable to get bind socket...\n");
+        return -1;
+    }
 
-    listen(sd, 10);
+    printf("listening...\n");
+    int err = listen(sd, 10);
+    if(err == -1){
+        printf("server: unable to listen...\n");
+        return -1;
+    }
 
     free(hints);
     freeaddrinfo(results);
@@ -27,6 +43,10 @@ int server_connect(int sd) {
     struct sockaddr_storage client_address;
     sock_size = sizeof(client_address);
     client_socket = accept(sd,(struct sockaddr *)&client_address, &sock_size);
+    if(client_socket == -1){
+        printf("error: %s\n", strerror(errno));
+        return -1;
+    }
   
     return client_socket;
 }
@@ -37,11 +57,23 @@ int client_connect() {
     hints->ai_family = AF_INET;
     hints->ai_socktype = SOCK_STREAM; //TCP socket
     hints->ai_flags = AI_PASSIVE; //only needed on server
-    getaddrinfo("127.0.0.1", "9845", hints, &results);  //Server sets node to NULL
+    int err = getaddrinfo("127.0.0.1", "9845", hints, &results);  //Server sets node to NULL
+    if(err == -1){
+        printf("client: unable to get IP info...\n");
+        return -1;
+    }
   
     int sd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
-    connect(sd, results->ai_addr, results->ai_addrlen);;
-
+    if(sd == -1){
+        printf("client: unable to make socket...\n");
+        return -1;
+    }
+    int err = connect(sd, results->ai_addr, results->ai_addrlen);;
+    if(err == -1){
+        printf("client: unable to connect to server...\n");
+        return -1;
+    }
+    
     free(hints);
     freeaddrinfo(results);
 
