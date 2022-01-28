9ef6bda4f9457c2c55fb24deb95c20f67a47c25a
dlin21 <90690772+dlin21@users.noreply.github.com>
2022-01-27 22:46:29 -0500

    Update basic_server.c

--- basic_server.c
+++ basic_server.c
@@ -5,21 +5,33 @@ int main() {
   
   while (1) {
     int from_client = server_connect(sd);
+    if(from_client == -1){
+      printf("client connection unavailable\n");
+      return 0;
+    }
+    
     int f = fork();
-       
     if (f) {} 
-        
     else { 
       char line[BUFFER_SIZE];
 
       while (1) { // Prompt client for input
         if (read(from_client, line, sizeof(line))==0) {
-          close(from_client);
+          int err = close(from_client);
+          if(err == -1){
+            printf("unable to read from client\n");
+          }
           break;
         }
+        
         for(int i=0; i<strlen(line); i++) line[i] = tolower(line[i]);
-        write(from_client, line, sizeof(line)); // send data to client
+        
+        int err = write(from_client, line, sizeof(line)); // send data to client
+        if(err == -1){
+          printf("unable to write to client\n");
+          return 0;
+        }
       }
+    }
   }
-  return 0;
 }
