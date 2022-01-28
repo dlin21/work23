4da95ceb9e41970186b513c9416ece5b27756e42
dlin21 <90690772+dlin21@users.noreply.github.com>
2022-01-27 23:02:06 -0500

    Update base_client.c

--- base_client.c
+++ base_client.c
@@ -1,25 +1,22 @@
 #include "pipe_networking.h"
 
-
 int main() {
     char line[BUFFER_SIZE];
-
     int sd = client_connect();
 
     while (1) {
-        // Prompts user for input
         printf("message: ");
         fgets(line, BUFFER_SIZE, stdin);
-        int err = write(sd, line, sizeof(line)); // send data to server
+        int err = write(sd, line, sizeof(line));
         if(err == -1){
             printf("unable to write to server...\n");
             return 0;
         }
-        int err = read(sd, line, sizeof(line)); // reads processed data from server
+        int err = read(sd, line, sizeof(line));
         if(err == -1){
             printf("unable to read from server...\n");
             return 0;
         }
-        printf("response: %s\n", line); // displays processed data to server
+        printf("response: %s\n", line);
     }
 }
